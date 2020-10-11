e4de85b9b0c0f872b92653f222019a0495b60836
RandyBeans448 <65494300+RandyBeans448@users.noreply.github.com>
2020-10-11 19:27:35 +0100

    Update UpdateCourse.js

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,57 +1,84 @@
 import React, { Component } from 'react';
+const ReactMarkdown = require('react-markdown')
 
 export class UpdateCourse extends Component {
-
+    
     state = {
         authenticatedUser: this.state,
         courses: [],
         errors: this.state,
+        courseDetails: []
       }
 
+      componentDidMount() {
+        const paramsId = this.props.match.params.id;
+        const parsedId = parseInt(paramsId);
+        this.props.context.data.getCoursesById(1).then((respsonse => {
+            if(respsonse) {
+                console.log('True')
+                this.setState({
+                    courseDetails: respsonse.course
+                })
+            }
+        })).catch(error => {
+            console.log('Course does not exsist', error)
+        })
+
+    }
+
+
     render () {
-        const { context } = this.props
-        console.log(this.props.context.courses)
+        let display = this.state.courseDetails;
+
+        function handleSubmit(event) {
+            //take the value of each input and textarea and then call updateUsers
+            let defaultValue = this.query.value;
+            event.preventDefault();
+            this.props.context.actions.updateUsers()
+            console.log("working")
+          }
+
         return (
-<div className="updateDiv">
-<form>
-<div className="updateDivLeft">
-<div>
-<h3>Course title</h3>
-<div >
-<input id="title" name="title" type="text" placeholder="Course title..." value="Build a Basic Bookcase"></input>
-</div>
-<p>By Joe Smith</p>
-</div>
-<div>
-<div>
-<textarea id="description" name="description" class="updateDivRight" placeholder="Course description..."></textarea>
-</div>
-</div>
-</div>
-<div>
-<div className="updateDivRight">
-<ul>
-<li>
-<h4>Estimated Time</h4>
-<div>
-<input id="estimatedTime" name="estimatedTime" type="text" placeholder="Hours" value="14 hours"></input>
-</div>
-</li>
-<li>
-<h4>Materials Needed</h4>
-<div>
-<textarea id="materialsNeeded" name="materialsNeeded" placeholder="List materials..."></textarea>
-</div>
-</li>
-</ul>
-</div>
-</div>
-<div >
-<button class="button" type="submit">Update Course</button>
-<button class="button button-secondary">Cancel</button>
-</div>
-</form>
-</div>
+            <div className="updateDiv">
+                <form>
+                    <div className="updateDivLeft">
+                        <div>
+                        <h3>Course title</h3>
+                            <div >
+                                <input id="title" name="title" type="text" defaultValue={display.title}></input>
+                            </div>
+                                <p>By Joe Smith</p>
+                        </div>
+                            <div>
+                                <div>
+                                   {/* <textarea id="description" name="description" className="updateDivRight" defaultValue={display.description}></textarea>*/}
+                                </div>
+                            </div>
+                    </div>
+                    <div>
+                        <div className="updateDivRight">
+                            <ul>
+                                <li>
+                                <h4>Estimated Time</h4>
+                                    <div>
+                                        {/*<input id="estimatedTime" name="estimatedTime" type="text" defaultValue={display.estimatedTime}></input>*/}
+                                    </div>
+                                </li>
+                                <li>
+                                <h4>Materials Needed</h4>
+                                    <div>
+                                        {/*<textarea id="materialsNeeded" name="materialsNeeded" defaultValue={display.materialsNeeded}></textarea>*/}
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div >
+                        <button onClick={handleSubmit} className="button" type="submit">Update Course</button>
+                        <button className="button button-secondary">Cancel</button>
+                    </div>
+                </form>
+            </div>
         )
     }
 }
