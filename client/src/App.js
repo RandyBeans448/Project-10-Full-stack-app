423080978d8cce56cac5b084d4e285c7a85d2593
RandyBeans448 <65494300+RandyBeans448@users.noreply.github.com>
2020-10-19 21:17:21 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -48,34 +48,15 @@ export default () => (
     <div>
     <HeaderWithContext/>
           <Switch>
-            <Route exact path="/" render={() => <Redirect to="/courses"/>}/>
+            <Route exact path="/" component={CoursesWithContext} />
+            <PrivateRoute path="/courses/create" component={CreateCourseWithContext} />
+            <Route exact path="/courses/:id" component={CourseDetailsWithContext} />
+            <PrivateRoute path="/courses/:id/update" component={UpdateCourseWithContext} />
             <Route path="/signin" component={UserSignInWithContext} />
             <Route path="/signup" component={UserSignUpWithContext} />
             <Route path="/signout" component={UserSignOutWithContext} />
-            <Route path="/courses" component={CoursesWithContext} />
-            <Route path="/:id" component={CourseDetailsWithContext} />
-            <PrivateRoute path="/create" component={CreateCourseWithContext} />
-            <PrivateRoute path="/:id/update" component={UpdateCourseWithContext} />
             <Route component={NotFound} />
           </Switch>
     </div>
   </Router>
 );
-
-// export default () => (
-//   <Router>
-//     <div>
-//     <HeaderWithContext/>
-//           <Switch>
-//             <Route path="/signin" component={UserSignInWithContext} />
-//             <Route path="/signup" component={UserSignUpWithContext} />
-//             <Route path="/signout" component={UserSignOutWithContext} />
-//             <Route exact path="/" component={CoursesWithContext} />
-//             <Route path="/coursedetail/:id" component={CourseDetailsWithContext} />
-//             <PrivateRoute path="/createcourse" component={CreateCourseWithContext} />
-//             <PrivateRoute path="/updateCourse/:id" component={UpdateCourseWithContext} />
-//             <Route component={NotFound} />
-//           </Switch>
-//     </div>
-//   </Router>
-// );
