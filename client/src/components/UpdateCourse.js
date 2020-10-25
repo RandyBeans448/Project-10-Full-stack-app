a14756eb17da6a56159157a387a7244c29220cb3
RandyBeans448 <65494300+RandyBeans448@users.noreply.github.com>
2020-10-25 12:11:21 +0000

    Update UpdateCourse.js

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -11,12 +11,12 @@ export class UpdateCourse extends Component {
             authenticatedUser: this.state,
             emailAddress: '',
             password: '',
+            userId: '',
             title: '',
             description: '',
             estimatedTime: '',
             materialsNeeded: '',
             errors: this.state,
-            courseDetails: []
           }
 
           this.handleSubmit = this.handleSubmit.bind(this);
@@ -26,13 +26,16 @@ export class UpdateCourse extends Component {
 
 
       componentDidMount() {
+
         const paramsId = this.props.match.params.id;
         const parsedId = parseInt(paramsId);
 
         const { context } = this.props; 
         const authedUser = context.authenticatedUser;
+        
         const emailAddress = authedUser.emailAddress;
         const password = authedUser.password;
+
 
         this.setState({
             emailAddress: emailAddress,
@@ -40,10 +43,15 @@ export class UpdateCourse extends Component {
         });
 
         this.props.context.data.getCoursesById(parsedId).then((respsonse => {
+
             if(respsonse) {
                 console.log('True');
                 this.setState({
-                    courseDetails: respsonse.course
+                    title: respsonse.course.title,
+                    description: respsonse.course.description,
+                    estimatedTime: respsonse.course.estimatedTime,
+                    materialsNeeded: respsonse.course.materialsNeeded,
+                    userId: respsonse.course.userId
                 })
             }
         })).catch(error => {
@@ -62,40 +70,50 @@ export class UpdateCourse extends Component {
 
     handleSubmit = (event) => {
 
+     const { context } = this.props;   
+
      const {
+            userId,
             title,
             description,
             estimatedTime,
             materialsNeeded 
             } = this.state;
-
+            
      const updatedCourse = {
+           userId,
            title,
            description,
            estimatedTime,
            materialsNeeded 
          };
 
+         
+
      const { emailAddress, password } = this.state;
 
         event.preventDefault();
+        console.log(updatedCourse)
 
-        this.props.context.data.updateCourse(updatedCourse ,emailAddress, password).then((respsonse => {
-            if(respsonse.status(201)) {
+        context.data.updateCourse(updatedCourse, emailAddress, password).then((respsonse => {
+            if(respsonse.status(204)) {
+                
                 console.log(respsonse);
             } else {
                 throw new Error
             }
-        })).catch(error => {
-            console.log('Course was not updated', error);
+        })).catch(errors => {
+            console.log('Course was not updated', errors);
+            this.setState({ errors });
+            console.log(this.state.errors);
         })
       };
 
 
     render () {
 
-        const display = this.state.courseDetails;
-        console.log(display);
+        console.log(this.state.title)
+        console.log(this.state.userId)
         const { context } = this.props; 
         const authedUser = context.authenticatedUser;
     
@@ -106,13 +124,13 @@ export class UpdateCourse extends Component {
                         <div  className="gridLeft" >
                           <h3>Course title</h3>
                             <div>
-                                <input id="title" name="title" type="text" onChange={this.handleChange} defaultValue={display.title}></input>
+                                <input id="title" name="title" type="text" onChange={this.handleChange} defaultValue={this.state.title}></input>
                             </div>
                                 <p>By {authedUser.firstName} {authedUser.lastName}</p>
                         </div>
                             <div>
                                 <div>
-                                   <textarea className="updateDivRight" id="description" name="description"  onChange={this.handleChange} defaultValue={display.description}></textarea>
+                                   <textarea className="updateDivRight" id="description" name="description"  onChange={this.handleChange} defaultValue={this.state.description}></textarea>
                                 </div>
                             </div>
                     </div>
@@ -122,13 +140,13 @@ export class UpdateCourse extends Component {
                                 <li>
                                 <h4 className="h4-update">Estimated Time</h4>
                                     <div>
-                                        <input className="update-right-input" id="estimatedTime" name="estimatedTime" type="text" onChange={this.handleChange} defaultValue={display.estimatedTime}></input>
+                                        <input className="update-right-input" id="estimatedTime" name="estimatedTime" type="text" onChange={this.handleChange} defaultValue={this.state.estimatedTime}></input>
                                     </div>
                                 </li>
                                 <li>
                                 <h4 className="h4-update">Materials Needed</h4>
                                     <div>
-                                        <textarea className="sideTextArea-2" id="materialsNeeded" name="materialsNeeded" onChange={this.handleChange} defaultValue={display.materialsNeeded}></textarea>
+                                        <textarea className="sideTextArea-2" id="materialsNeeded" name="materialsNeeded" onChange={this.handleChange} defaultValue={this.state.materialsNeeded}></textarea>
                                     </div>
                                 </li>
                             </ul>
