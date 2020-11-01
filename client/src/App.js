a1ec2ef9dc29ed9b8481f90933a92c64102092c1
RandyBeans448 <65494300+RandyBeans448@users.noreply.github.com>
2020-11-01 21:16:46 +0000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,9 @@ import {
   Switch
 } from 'react-router-dom';
 
-import './App.css';
 
+import './App.css';
+import ParticlesContainer from './components/Particles';
 import CourseDetail from './components/CourseDetail';
 import Courses from './components/Courses';
 import CreateCourse from './components/CreateCourse';
@@ -45,18 +46,20 @@ const CreateCourseWithContext = withContext(CreateCourse)
 
 export default () => (
   <Router>
-    <div>
+    <div >
     <HeaderWithContext/>
-          <Switch>
-            <Route exact path="/" component={CoursesWithContext} />
-            <PrivateRoute path="/courses/create" component={CreateCourseWithContext} />
-            <Route exact path="/courses/:id" component={CourseDetailsWithContext} />
-            <PrivateRoute path="/courses/:id/update" component={UpdateCourseWithContext} />
-            <Route path="/signin" component={UserSignInWithContext} />
-            <Route path="/signup" component={UserSignUpWithContext} />
-            <Route path="/signout" component={UserSignOutWithContext} />
-            <Route component={NotFound} />
-          </Switch>
-    </div>
+      <Switch>
+        <Route exact path="/" component={CoursesWithContext} />
+        <PrivateRoute path="/courses/create" component={CreateCourseWithContext} />
+        <Route exact path="/courses/:id" component={CourseDetailsWithContext} />
+        <PrivateRoute path="/courses/:id/update" component={UpdateCourseWithContext} />
+        <Route path="/signin" component={UserSignInWithContext} />
+        <Route path="/signup" component={UserSignUpWithContext} />
+        <Route path="/signout" component={UserSignOutWithContext} />
+        <Route component={NotFound} />
+      </Switch >
+    </div >
   </Router>
+  
 );
+
