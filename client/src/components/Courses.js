221a9c99a5d17d0dfa6349208c66a9caddbe094b
RandyBeans448 <65494300+RandyBeans448@users.noreply.github.com>
2020-11-07 15:05:39 +0000

    Update Courses.js

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -9,10 +9,16 @@ class Courses extends React.Component {
       errors: [],
     }
 
+    // GET request called to get the full list of active courses 
+
     componentDidMount() {
 
+    // Destructuring context to access the usersCourses function    
+
+      const { context } = this.props
+
       this.setState({
-          courses: this.props.context.actions.usersCourses()
+          courses: context.actions.usersCourses()
       })
 
   }
@@ -20,26 +26,24 @@ class Courses extends React.Component {
   render() {
 
     const { context } = this.props; 
-    const authedUser = context.authenticatedUser;
-    let mappedCourses;
-    let noAuthedUser;
 
-    if(this.props.context.courses !== null) {
-      
-        mappedCourses = this.props.context.courses.courses.map ((course) =>
-        <div className="table-cell" key={course.id}> 
+    /*
+    Cycling through the courses retrieved from the GET request.
+    For each courses a div created with a NavLink to the
+    relative detail page.
+
+    mappedCourses stores this and used to display the results in 
+    the return render method.
+    */
+
+    let mappedCourses;
+
+    if(context.courses !== null) {
+        mappedCourses = context.courses.courses.map ((course) =>
+       <div className="table-cell" key={course.id}> 
         <p>Course</p> 
         <NavLink to={`/courses/${course.id}`}> {course.title} </NavLink>
       </div>
-    )
-  } else if (this.props.context.courses !== null && authedUser === false) {
-    
-    noAuthedUser = this.props.context.courses.courses.map ((course) =>
-      <div className="table-cell" key={course.id}> 
-        <p>Course</p> 
-        <NavLink to="/signin"> {course.title} </NavLink>
-      </div>
-
     )
   } else {
       return (
@@ -48,13 +52,12 @@ class Courses extends React.Component {
         </div>
     )
   } 
-  
+
     return (
       <div id="tsparticles" className="tsparticles">
         <div className="course-list-div">
           <div className="table">  
               {mappedCourses}
-              {noAuthedUser}
             <div className="table-cell">
               <NavLink to="/courses/create"> + New Course </NavLink>
             </div>
@@ -62,7 +65,6 @@ class Courses extends React.Component {
         </div>
         <ParticlesContainer/>
       </div>
-      
     );
   }
 }
