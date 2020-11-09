12c38b4a470f97918f564de1a975f3b2c81573b8
RandyBeans448 <65494300+RandyBeans448@users.noreply.github.com>
2020-11-09 19:09:54 +0000

    Update PrivateRoute.js

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Route
-  } from 'react-router-dom';
+import { Route } from "react-router-dom";
 import { Redirect } from 'react-router-dom';
 import { Consumer } from './Context';
 
