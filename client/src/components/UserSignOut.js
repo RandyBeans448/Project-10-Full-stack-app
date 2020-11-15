25ef425990d113361249817d3644119144c90414
RandyBeans448 <65494300+RandyBeans448@users.noreply.github.com>
2020-11-15 13:46:32 +0000

    Update UserSignOut.js

--- client/src/components/UserSignOut.js
+++ client/src/components/UserSignOut.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Component } from "react";
 import { Redirect } from 'react-router-dom';
 
-export default ({ context }) => {
-    context.actions.signOut();
-    return (
-      < Redirect to="/" />
-    );
-  }
+// export default ({ context }) => {
+//     context.actions.signOut();
+//     return (
+//       < Redirect to="/" />
+//     );
+//   }
+
+  export class UserSignOut extends Component {
+
+    componentDidMount() {
+      const { context } = this.props;
+      context.actions.signOut();
+    }
+
+    render() {
+      return(
+        < Redirect to="/" />
+      )
+    }
+    
+  } 
+
+  export default UserSignOut;
+
